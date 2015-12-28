58ed9113dcdc07fce379f7475599be3edd27086a
achesak <achesak@yahoo.com>
2015-12-28 12:32:12 -0600

    Initial commit.

---
+++ fnmatch.nim
@@ -0,0 +1,139 @@
+# Nim module for filename matching with UNIX shell patterns.
+# Based on Python's fnmatch module.
+
+# Written by Adam Chesak.
+# Released under the MIT open source license.
+
+
+## fnmatch is a Nim module for filename matching with UNIX shell patterns. It is based on 
+## Python's fnmatch module in the standard library.
+##
+## Examples:
+##
+## .. code-block:: nimrod
+##    
+##    # Test if a filename matches a pattern, ignoring case.
+##    var filename : string = "EXAMPLE.TXT"
+##    var pattern : string = "*.txt"
+##    var matches : bool = fnmatch(filename, pattern)
+##    echo(matches) # outputs true
+##
+## .. code-block:: nimrod
+##    
+##    # Test if a filename matches a pattern, taking into account case.
+##    # Using same filename and pattern as previous example.
+##    matches = fnmatchcase(filename, pattern)
+##    echo(matches) # outputs false
+##
+## .. code-block:: nimrod
+##
+##    # Filter a list of names to get a subset that matches a pattern.
+##    var names : seq[string] = @["list", "of", "test.txt", "FILES.TXT", "fnmatch.nim", "fnmatch.testfile"]
+##    var filtered : seq[string] = filter(names, "*.t*)
+##    echo(filtered)
+##    # outputs @["test.txt, "FILES.TXT", "fnmatch.testfile"]
+##    
+##    # filter() can also filter based on case, if true is given as the third parameter.
+##    filtered = filter(names, "*.t*", true)
+##    echo(filtered)
+##    # outputs @["test.txt", "fnmatch.testfile"]
+
+
+import strutils
+import re
+
+
+proc fnmatchEscapeRe(s: string): string =
+    ## Internal proc. Escapes `s` so that it is matched verbatim when used as a regular
+    ## expression. Based on the ``escapeRe`` proc in the re module in the Nim standard library.
+    var escaped = ""
+    for c in items(s):
+        case c
+        of 'a'..'z', 'A'..'Z', '0'..'9', '_':
+            escaped.add(c)
+        else:
+            escaped.add("\\" & c)
+    return escaped
+
+
+proc translate*(pattern : string): string = 
+    ## Returns the shell-style ``pattern`` converted to a regular expression.
+    
+    var i : int = 0
+    var j : int = 0
+    var n : int = len(pattern)
+    var c : string = ""
+    var inside : string = ""
+    var result : string = ""
+    
+    while i < n:
+        c = "" & pattern[i]
+        i += 1
+        
+        if c == "*":
+            result &= ".*"
+        
+        elif c == "?":
+            result &= "."
+        
+        elif c == "[":
+            j = i
+            
+            if j < n and pattern[j] == '!':
+                j += 1
+            if j < n and pattern[j] == ']':
+                j += 1
+            
+            while j < n and pattern[j] != ']':
+                j += 1
+            
+            
+            if j >= n:
+                result &= "\\["
+            else:
+                inside = pattern[i..j+1].replace("\\", "\\\\")
+                i = j + 1
+                
+                if inside[0] == '!':
+                    inside = "^" & inside[1..high(inside)]
+                elif inside[0] == '^':
+                    inside = "\\" & inside
+                
+                result = result & "[" & inside & "]"
+        
+        else:
+            result &= fnmatchEscapeRe(c)
+
+    return result & "\\Z(?ms)"
+
+
+proc fnmatch*(filename : string, pattern : string): bool = 
+    ## Tests whether ``filename`` matches ``pattern``, returning ``True`` or ``False``.
+    
+    var f : string = filename.toLower()
+    var p : string = pattern.toLower()
+    
+    return re.match(f, re(translate(p)))
+
+
+proc fnmatchcase*(filename : string, pattern : string): bool = 
+    ## Tests whether ``filename`` matches ``pattern``, returning ``True`` or ``False``;
+    ## the comparison is case-sensitive.
+    
+    return re.match(filename, re(translate(pattern)))
+
+
+proc filter*(names : seq[string], pattern : string, casesensitive : bool = false): seq[string] = 
+    ## Returns the subset of the list of ``names`` that match ``pattern``.
+    
+    var r : seq[string] = @[]
+    
+    for name in names:
+        if casesensitive:
+            if fnmatchcase(name, pattern):
+                r.add(name)
+        else:
+            if fnmatch(name, pattern):
+                r.add(name)
+    
+    return r
